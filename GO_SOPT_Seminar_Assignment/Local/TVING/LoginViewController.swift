d97c78741f6f536c5450700221d06df19add0934
Daye Kim <dykim315@naver.com>
2023-04-24 04:50:26 +0900

    [초기 세팅] 폴더링 및 에셋 추가

---
+++ GO_SOPT_Seminar_Assignment/Local/TVING/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  GO_SOPT_Seminar_Assignment
+//
+//  Created by 김다예 on 2023/04/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- GO_SOPT_Seminar_Assignment/SceneDelegate.swift
+++ GO_SOPT_Seminar_Assignment/SceneDelegate.swift
@@ -1,22 +1,22 @@
 //
 //  SceneDelegate.swift
-//  GO_SOPT_Seminar_Assignment
+//  GO_SOPT_Seminar_Assingment
 //
-//  Created by 김다예 on 2023/04/24.
+//  Created by 김다예 on 2023/04/07.
 //
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+            
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            let navigationController = UINavigationController(rootViewController: LoginViewController())
+            self.window = UIWindow(windowScene: windowScene)
+            self.window?.rootViewController = navigationController
+            self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -49,4 +49,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
-
